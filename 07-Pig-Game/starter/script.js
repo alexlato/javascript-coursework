46b5bc55533b807b4d90821371275d99dbb76be2
Alex LaTourelle <latourellealex@yahoo.com>
2023-09-29 18:19:30 -0700

    pig game initial var

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,14 +1,14 @@
 'use strict';
 
 // Selecting elements
-const player0E1 = document.querySelector('.player--0');
-const player1E1 = document.querySelector('.player--1');
-const score0E1 = document.querySelector('#score--0');
-const score1E1 = document.querySelector('.score--1');
-const current0E1 = document.querySelector('.current--0');
-const current1E1 = document.querySelector('.current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('.score--1');
+const current0El = document.querySelector('.current--0');
+const current1El = document.querySelector('.current--1');
 
-const diceE1 = document.querySelector('.dice');
+const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
@@ -21,16 +21,16 @@ const init = function () {
   currentScore = 0;
   (activePlayer = 0), (playing = true);
 
-  score0E1.textContent = 0;
-  score1E1.textContent = 0;
-  current0E1.textContent = 0;
-  current1E1.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
 
-  diceE1.classList.add('hidden');
-  player0E1.classList.remove('player--winner');
-  player1E1.classList.remove('player-winner');
-  player0E1.classList.add('palyer-active');
-  player1E1.classList.remove('remove--active');
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player-winner');
+  player0El.classList.add('palyer-active');
+  player1El.classList.remove('remove--active');
 };
 
 init();
